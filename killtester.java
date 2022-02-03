cd82160b0901ad6045640b59beb825c41bd889c6
Ripslash <92413947+Ripslash@users.noreply.github.com>
2022-02-02 20:55:11 -0800

    the tertiary fix

--- killtester.java
+++ killtester.java
@@ -1,8 +1,10 @@
 import static org.junit.Assert.*;
 import org.junit.*;
 
-@Test
 public abstract class killtester {
+
+    
+    @Test
     public void test() {
         assertEquals((int) (killdemo.killme()), 1);
     }
